809a6886e7506ccaaf213ea36b41948a938f2841
MihailTs <mihail.tsvetkov.04@gmail.com>
2023-04-28 19:04:14 +0300

    Integer added

--- Integer.cpp
+++ Integer.cpp
@@ -1,9 +1,88 @@
 #include "Integer.h"
 
-std::string Integer::getTypeName(){
-    return "Integer";
+Integer::Integer(){
+    setValue(0);
 }
 
-int Integer::getValue(){
+Integer::Integer(int _value){
+    setValue(_value);
+}
+
+Integer::Integer(const std::string& _strValue){
+    setValue(stringToInt(_strValue));
+}
+
+std::string Integer::getTypeName(){
+    return "int";
+}
+
+int Integer::getValue() const{
     return value;
+}
+
+void Integer::setValue(int newVal){
+    value = newVal;
+    setStringValue(std::to_string(newVal));
+}
+
+std::ostream& operator<<(std::ostream& out, const Integer& number){
+    out << number.getValue();
+    return out;
+}
+
+std::fstream& operator<<(std::fstream& out, const Integer& number){
+    out << number.getStringValue().c_str();
+    return out;
+}
+
+Integer& Integer::operator=(const Integer& other){
+    setValue(other.getValue());
+    return *this;
+}
+
+bool Integer::isValid(const std::string& _strValue){
+    unsigned size = _strValue.size();
+    char firstChar = _strValue[0];
+
+    //случаи като '+', '-', 'm',...
+    if(size == 1 && (firstChar < '0' || firstChar > '9')) return false;
+
+    //случаи, в които първият символ е невалиден
+    if(firstChar != '-' && firstChar != '+' &&
+        (firstChar <= '0' || firstChar > '9')) return false;
+
+    //случаи като '+012',...
+    if((firstChar == '-' || firstChar == '+') && _strValue[1] == '0' && size > 2) 
+        return false;
+
+    //проверка за валидност на символите
+    for(int i = 1; i < size; i++)
+        if(_strValue[i] < '0' || _strValue[i] > '9') return false;
+
+    return true;
+}
+
+int Integer::myPower(int number, int power){
+    int product = 1;
+    for(int i = 1; i <= power; i++)
+        product *= number;
+    return product;
+}
+
+int Integer::stringToInt(const std::string& _strValue){
+    
+    if(!isValid(_strValue)) 
+        throw std::invalid_argument(_strValue + " is not a valid int number!");
+    
+    int numValue = 0;
+    
+    int power = 0;
+    for(int i = _strValue.size()-1; i > 0; i--)
+        numValue += (_strValue[i]-'0') * myPower(10, power++);
+
+    if(_strValue[0] == '+') return numValue;
+    if(_strValue[0] == '-') return -1*numValue;
+    numValue += (_strValue[0]-'0') * myPower(10, power++);
+
+    return numValue;
 }
--- Integer.h
+++ Integer.h
@@ -1,16 +1,40 @@
 #pragma once
+#include <fstream>
+#include <iostream>
 #include <string>
-#include "NumericType.h"
+#include <stdexcept>
+#include "DataType.h"
 
-class Integer : public NumericType{
+class Integer : public DataType{
 
     private:
         int value;
 
+        bool isValid(const std::string&);
+
+        int myPower(int, int);
+
+        int stringToInt(const std::string&);
+
     public:
+
+        Integer();
+
+        Integer(int);
+
+        Integer(const std::string&);
 
         std::string getTypeName();
 
-        int getValue();
+        int getValue() const;
 
+        void setValue(int);
+
+        Integer& operator=(const Integer&);
+
+        Integer& operator=(const std::string&);
+
+        friend std::ostream& operator<<(std::ostream&, const Integer&);
+
+        friend std::fstream& operator<<(std::fstream&, const Integer&);
 };
