0f9a8fd58a2077dcd20c435c3abc4997f0d77556
MihailTs <mihail.tsvetkov.04@gmail.com>
2023-05-02 17:41:19 +0300

    TableRow -> TableCol

---
+++ TableCol.cpp
@@ -0,0 +1,31 @@
+#include "TableCol.h"
+
+TableCol::TableCol(){
+    setValuesCount(0);
+}
+
+TableCol::TableCol(const std::vector<DataType*>& _values){
+    setValues(_values);
+    setValuesCount(_values.size());
+}
+
+void TableCol::setValues(const std::vector<DataType*>& _values){
+    values = _values;
+    setValuesCount(_values.size());
+}
+
+std::vector<DataType*> TableCol::getValues(){
+    return values;
+}
+
+unsigned TableCol::getValuesCount(){
+    return getValues().size();
+}
+
+void TableCol::setValuesCount(unsigned count){
+    valuesCount = count;
+}
+
+void TableCol::addValue(DataType* _value){
+    getValues().push_back(_value);
+}
---
+++ TableCol.h
@@ -0,0 +1,30 @@
+#include <vector>
+#include <fstream>
+#include "DataType.h"
+
+class TableCol{
+
+    private:
+
+        std::vector<DataType*> values;
+        unsigned valuesCount;
+
+        void setValuesCount(unsigned);
+
+    public:
+
+        TableCol();
+
+        TableCol(const std::vector<DataType*>&);
+
+        std::vector<DataType*> getValues();
+
+        //На практика връща броят на редовете в таблицата
+        unsigned getValuesCount();
+
+        //Добавя нова стойност към колоната
+        void addValue(DataType*);
+
+        void setValues(const std::vector<DataType*>&);
+
+};
